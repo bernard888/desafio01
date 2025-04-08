254f5b201262e8075a23928752057380c75ff6bf
Bernard <bernardd.lego274@gmail.com>
2025-04-08 08:54:57 -0300

    desafio01 corrigido com o professor em sala

--- desafio01.py
+++ desafio01.py
@@ -3,16 +3,16 @@ m1 = int(input("digite os minutos dentro dessa hora: "))
 h2 = int(input("coloque uma hora do dia: "))
 m2 = int(input("digite os minutos dentro dessa hora: "))
 
-hf = h1+h2
-mf = m1+m2
-if mf >=60:
-   mf = mf % 60
-   hf = hf+1
-else:
-    mf=mf
-if hf>=13:
-    hf = hf-24
-else:
-    hf = hf
+somah = h1+h2
+somam = m1+m2
 
-print(f"{hf}:{mf}")
+if somam>59:
+    somah += 1
+    somam %= 60
+if somah >36:
+    somah-=36
+elif somah>=24:
+    somah-=24
+elif somah>=12:
+    somah-= 12
+print(f"{somah}:{somam}")
